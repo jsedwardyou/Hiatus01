8a44d739941e03521d020f45015605781ae81244
Edward <jsedwardyou@gmail.com>
2018-08-30 00:04:34 +0900

    Mosquito movement 제작중

---
+++ Hiatus01/Assets/_Script/mosquito_movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mosquito_movement : MonoBehaviour {
+
+    [SerializeField] private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float forward_movement = Input.GetAxis("Vertical");
+
+        transform.Translate(transform.forward * speed * Time.deltaTime * forward_movement);
+	}
+}
