4ddf6e0adfb0e14a0170ddcce7eedfbc4e2dc3ac
Edward <jsedwardyou@gmail.com>
2018-08-30 02:43:42 +0900

    Mosquito 움직임 + 붙기 제작

--- Hiatus01/Assets/_Script/mosquito_movement.cs
+++ Hiatus01/Assets/_Script/mosquito_movement.cs
@@ -1,20 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class mosquito_movement : MonoBehaviour {
 
+    [SerializeField] private Camera fp_cam;
+    [SerializeField] private Camera tp_cam;
+
     [SerializeField] private float speed;
+    [SerializeField] private float jump_force;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private Rigidbody rb;
+    private bool canMove = true;
+
+    // Use this for initialization
+    void Start() {
+        mouselook.Init(transform, fp_cam.transform);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
         float forward_movement = Input.GetAxis("Vertical");
+        float right_movement = Input.GetAxis("Horizontal");
 
-        transform.Translate(transform.forward * speed * Time.deltaTime * forward_movement);
-	}
+        if (canMove)
+        {
+            transform.Translate(Vector3.forward * speed * forward_movement * Time.deltaTime);
+            transform.Translate(Vector3.right * speed * right_movement * Time.deltaTime);
+        }
+        else {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Switch_To_FirstPerson();
+            }
+        }
+         
+        if (Input.GetKey(KeyCode.Space)) {
+            rb.AddForce(Vector3.up * jump_force);
+        }
+
+        LookAt_Mouse_Position();
+
+    }
+
+    [SerializeField] private MouseLook mouselook = new MouseLook();
+    private void LookAt_Mouse_Position() {
+        mouselook.LookRotation(transform, fp_cam.transform);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Switch_To_ThirdPerson();
+    }
+
+
+
+    private void Switch_To_FirstPerson() {
+        tp_cam.depth = -1;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX;
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+
+        canMove = true;
+    }
+    private void Switch_To_ThirdPerson()
+    {
+        tp_cam.depth = 1;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+
+        canMove = false;
+    }
 }
